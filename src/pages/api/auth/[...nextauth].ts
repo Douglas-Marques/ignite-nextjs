457bcfb338aca615d7781219614aa63b43c914c0
Douglas-Marques <douglasmarques09@gmail.com>
2021-08-05 22:57:45 -0300

    feat: add FaunaDB conexion

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -8,5 +12,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     })
-  ]
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true
+      } catch (e) {
+        console.error(e)
+        return false
+      }
+    }
+  }
 })
